d1e7172c5be32420f8b278ac96a41a874ed9502b
Changbum97 <chb2005@naver.com>
2023-08-08 17:51:43 +0900

    feat: Swagger Docs 적용

--- src/main/java/com/wanted/wantedpreonboardingbackend/config/SwaggerConfig.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/config/SwaggerConfig.java
@@ -1,2 +1,32 @@
-package com.wanted.wantedpreonboardingbackend.config;public class Swaggerconfig {
+package com.wanted.wantedpreonboardingbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.wanted.wantedpreonboardingbackend.controller.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("원티드 백엔드 프리온보딩 사전과제 - 안창범")
+                .description("[배포 사이트](http://ec2-52-79-213-143.ap-northeast-2.compute.amazonaws.com:8080)")
+                .version("1.0")
+                .build();
+    }
 }
