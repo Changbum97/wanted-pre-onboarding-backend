e6f745844df07f0207aec152ac7cbfdabc7958d0
Changbum97 <chb2005@naver.com>
2023-08-08 18:36:58 +0900

    feat: DTO 수정

--- src/main/java/com/wanted/wantedpreonboardingbackend/domain/dto/board/BoardDto.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/domain/dto/board/BoardDto.java
@@ -3,6 +3,8 @@ package com.wanted.wantedpreonboardingbackend.domain.dto.board;
 import com.wanted.wantedpreonboardingbackend.domain.entity.Board;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
@@ -14,6 +16,8 @@ public class BoardDto {
     private String userEmail;
     private String title;
     private String body;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedAt;
 
     public static BoardDto of(Board board) {
         return BoardDto.builder()
@@ -21,6 +25,8 @@ public class BoardDto {
                 .userEmail(board.getUser().getEmail())
                 .title(board.getTitle())
                 .body(board.getBody())
+                .createdAt(board.getCreatedAt())
+                .lastModifiedAt(board.getLastModifiedAt())
                 .build();
     }
 }
--- src/main/java/com/wanted/wantedpreonboardingbackend/domain/dto/user/UserLoginRequest.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/domain/dto/user/UserLoginRequest.java
@@ -4,6 +4,9 @@ import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginRequest {
 
     private String email;
