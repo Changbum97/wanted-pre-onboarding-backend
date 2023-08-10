fb09800202665830eedb15a41ec2272040b12933
Changbum97 <chb2005@naver.com>
2023-08-10 17:21:56 +0900

    fix: 프로젝트 Timezone 변경

--- src/main/java/com/wanted/wantedpreonboardingbackend/WantedPreOnboardingBackendApplication.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/WantedPreOnboardingBackendApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class WantedPreOnboardingBackendApplication {
 
@@ -14,6 +17,12 @@ public class WantedPreOnboardingBackendApplication {
             // Page를 1페이지 부터 시작하도록 설정
             p.setOneIndexedParameters(true);
         };
+    }
+
+    @PostConstruct
+    public void setTimeZone() {
+        // 프로젝트의 TimeZone을 서울로 맞춰줌
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
     public static void main(String[] args) {
